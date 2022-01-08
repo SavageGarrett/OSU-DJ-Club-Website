2e641a9905a4bbfb7f3f91c677867e7bdcd208bc
SavageGarrett <carder.320@osu.edu>
2022-01-08 02:01:51 -0500

    html_file error fixed

--- generate-content.js
+++ generate-content.js
@@ -6,8 +6,8 @@ let converter = new showdown.Converter();
 /* Load Config Files */
 const home_page_data = require('./content/home-page.json');
 
-/* gen_home('Home.html');
-gen_home('index.html'); */
+gen_home('Home.html');
+gen_home('index.html');
 gen_learn();
 
 /* Generate Learn-to-DJ page */
@@ -27,7 +27,7 @@ function gen_learn()
 
             /* Constant Selectors */
             const CONTAINER = 'div.u-expanded-width';
-            
+
             
             /* Log Success Message */
             console.log(`Data replaced for ${html_file}`);
