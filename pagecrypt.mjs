b697e9cc16d2296f911d7d19180343607ae003e7
SavageGarrett <carder.320@osu.edu>
2022-01-17 23:29:19 -0500

    Members Password Done
    Height Message Updated for From our DJs

--- pagecrypt.mjs
+++ pagecrypt.mjs
@@ -1,7 +1,8 @@
 import { encrypt } from 'pagecrypt'
+import password_page_data from './content/member-resources.json';
 
 try {
-    await encrypt('./public/Members.html', './public/Members.html', 'password123');
+    await encrypt('./public/Members.html', './public/Members.html', password_page_data["password"]);
 } catch (error) {
     console.log(error)
 }
--- update_login_form.js
+++ update_login_form.js
@@ -1,8 +1,45 @@
 const fs = require('fs');
+const password_page_data = require('./content/member-resources.json');
 
 gen_login();
 
 function gen_login()
 {
+    fs.readFile('./public/Members.html', 'utf-8', function (err, data) {
+        /* Throw Error to Avoid Malformed Content */
+        if (err) throw err;
+
+        /* Load HTML data */
+        let $ = cheerio.load(data);
+
+        /* 
+            Constant Selectors
+        */
+        const TAB_TITLE = 'title'
+        const PASSWORD_MESSAGE = '#msg'
+        const PAGECRYPT = '.fixed'
+
+        /* 
+            Update Content
+        */
+
+        /* Update Tab Title */
+        $(TAB_TITLE).html("Access Member Resources")
+
+        /* Update Password Prompt */
+        $(PASSWORD_MESSAGE).html(password_page_data["password-prompt"])
+
+        /* Pagecrypt */
+        $(PAGECRYPT).html('');
+
+        /* Write out Modified HTML */
+        fs.writeFile('./public/Members.html', $.html(), function (err) {
+            /* Throw Error to Avoid Malformed Content */
+            if (err) throw err;
+
+            /* Log Success Message */
+            console.log(`Data replaced for Members.html password Page`);
+        });
+    })
 
 }
