ad606b15dbee545dd05c4cfa72611c91e00a4c82
SavageGarrett <carder.320@osu.edu>
2022-01-17 23:34:39 -0500

    I LOVE JS MODULES
    Manually Parse JSON Data for Password Page

--- pagecrypt.mjs
+++ pagecrypt.mjs
@@ -1,8 +1,11 @@
 import { encrypt } from 'pagecrypt'
-import password_page_data from './content/member-resources.json';
+import fs from 'fs'
+
+let password_data_string = fs.readFileSync('./content/member-resources.json', 'utf-8');
+let password_data = JSON.parse(password_data_string);
 
 try {
-    await encrypt('./public/Members.html', './public/Members.html', password_page_data["password"]);
+    await encrypt('./public/Members.html', './public/Members.html', password_data['password']);
 } catch (error) {
     console.log(error)
 }
